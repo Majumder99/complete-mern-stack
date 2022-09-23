c076c3c4b2e99678b92ffe8d420eab8cce350339
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-24 00:41:27 +0600

    post method using async/await

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -4,19 +4,51 @@ const homeGet = (req, res) => {
   res.send("Hello from the user route");
 };
 
-const registerPost = (req, res) => {
+// const registerPost = (req, res) => {
+//   //   console.log(req.body);
+//   //   res.send({ msg: req.body });
+//   const { name, email, phone, work, password, repassword } = req.body;
+//   if (!name || !email || !phone || !work || !password || !repassword) {
+//     res.send({ msg: "Please fill the all fields" });
+//   }
+//   User.findOne({ email })
+//     .then((userExist) => {
+//       if (userExist) {
+//         console.log("user already exist");
+//       }
+//       const user = new User({
+//         name,
+//         email,
+//         phone,
+//         work,
+//         password,
+//         repassword,
+//       });
+//       user
+//         .save()
+//         .then(() =>
+//           res.status(201).json({ msg: "New user registered successfully" })
+//         )
+//         .catch((e) => console.log(e));
+//     })
+//     .catch((e) => console.log(e));
+// };
+
+//Using async/await
+const registerPost = async (req, res) => {
   //   console.log(req.body);
   //   res.send({ msg: req.body });
   const { name, email, phone, work, password, repassword } = req.body;
+
   if (!name || !email || !phone || !work || !password || !repassword) {
     res.send({ msg: "Please fill the all fields" });
   }
-  User.findOne({ email })
-    .then((userExist) => {
-      if (userExist) {
-        console.log("user already exist");
-      }
-      const user = new User({
+  try {
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      res.status(522).json({ msg: "User already exist" });
+    } else {
+      const user = await User.create({
         name,
         email,
         phone,
@@ -24,14 +56,15 @@ const registerPost = (req, res) => {
         password,
         repassword,
       });
-      user
-        .save()
-        .then(() =>
-          res.status(201).json({ msg: "New user registered successfully" })
-        )
-        .catch((e) => console.log(e));
-    })
-    .catch((e) => console.log(e));
+      if (user) {
+        res.status(201).json({ msg: "User created" });
+      } else {
+        res.status(500).json({ msg: "User cannot be created" });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
 module.exports = {
